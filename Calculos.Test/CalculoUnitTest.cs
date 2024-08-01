001a862cd406ed897f22a7302a736944049edd4e
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-08-01 09:51:29 -0300

    Adicionar arquivos de projeto.

---
+++ Calculos.Test/CalculoUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Calculos.Test
+{
+    public class CalculoUnitTest
+    {
+        /// <summary>
+        ///  AAA : Act, Arrange, Assert
+        ///  AAA : Agir , Organizar e Assertir
+        /// </summary>
+        [Fact] // notation para identifcar que o metodo é um metodo de test
+        public void TestSumMetodh()
+        {
+            // Arrange : Organizar
+            var x1 = 4.1;
+            var x2 = 5.9;
+            var result = 10;
+
+            // Act : Agir
+            var soma = Calculo.Somar(x1,x2);
+
+            // Assert : Provar
+            Assert.Equal(result, soma);
+        }
+    }
+}
